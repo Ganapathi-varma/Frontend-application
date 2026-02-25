a62502dbf11f51299571e933cf67196c1fa82694
Ganapathi-varma <vinayvarma508@gmail.com>
2026-02-25 10:55:38 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1> Sample image frontend page 🚀</h1>
+      <p> Ganapathi Varma .</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
